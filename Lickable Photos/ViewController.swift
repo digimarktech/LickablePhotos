2fb8fc1647534119c5921850f011f2c0678db191
Marc Aupont <mdaupont@gmail.com>
2018-10-26 16:20:14 -0400

    Initial Commit

---
+++ Lickable Photos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lickable Photos
+//
+//  Created by Marc Aupont on 10/26/18.
+//  Copyright © 2018 Marc Aupont. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
