fd0e19f84682a3e440b8544ce0f116ba05acc854
Marc Aupont <mdaupont@gmail.com>
2018-10-26 23:10:30 -0400

    added PhotoDetailVC

---
+++ Lickable Photos/Controller/PhotoDetailVC.swift
@@ -0,0 +1,19 @@
+//
+//  PhotoDetailVC.swift
+//  Lickable Photos
+//
+//  Created by Marc Aupont on 10/26/18.
+//  Copyright © 2018 Marc Aupont. All rights reserved.
+//
+
+import UIKit
+
+class PhotoDetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
