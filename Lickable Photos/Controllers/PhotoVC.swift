b433dd486d9c8a9c99ce69ae351ce8d8a94c80f1
Marc Aupont <mdaupont@gmail.com>
2018-10-31 09:33:38 -0400

    added empty data state management
    refactored codebase to make use of scenarios where there is no data or app is offline. View controller has started to grow and could use some refactoring to remove view code out

--- Lickable Photos/Controllers/PhotoVC.swift
+++ Lickable Photos/Controllers/PhotoVC.swift
@@ -9,6 +9,7 @@
 import UIKit
 import SDWebImage
 import Lottie
+import DZNEmptyDataSet
 
 final class PhotoVC: UIViewController {
 	
@@ -21,6 +22,19 @@ final class PhotoVC: UIViewController {
 	private let minimumLineSpacing: CGFloat = 10
 	private let minimumInteritemSpacing: CGFloat = 10
 	private let cellsPerRow = 3
+	
+	//State Management Properties
+	private var isFetchingData: Bool = false
+	private var isOffline: Bool = false
+	
+	private var activityIndicator: UIActivityIndicatorView = {
+		let activity = UIActivityIndicatorView()
+		activity.style = .whiteLarge
+		activity.hidesWhenStopped = true
+		activity.translatesAutoresizingMaskIntoConstraints = false
+		activity.color = UIColor(red: 178/255, green: 19/255, blue: 35/255, alpha: 1)
+		return activity
+	}()
 	
 	//UIRefreshControl
 	private(set) lazy var refreshControl: UIRefreshControl = {
@@ -47,8 +61,19 @@ final class PhotoVC: UIViewController {
 		collectionView.collectionViewLayout = layout
 		collectionView.delegate = self
 		collectionView.dataSource = dataSource
+		collectionView.emptyDataSetSource = self
+		collectionView.emptyDataSetDelegate = self
 		collectionView.refreshControl = refreshControl
-
+		
+		//Setup constraints for ActivityIndicator
+		view.addSubview(activityIndicator)
+		NSLayoutConstraint.activate([
+			activityIndicator.widthAnchor.constraint(equalToConstant: 45),
+			activityIndicator.heightAnchor.constraint(equalToConstant: 45),
+			activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+			activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+		])
+		
 		//Setup constraints for CustomRefreshView
 		refreshControl.addSubview(customRefreshView)
 		NSLayoutConstraint.activate([
@@ -58,20 +83,20 @@ final class PhotoVC: UIViewController {
 			customRefreshView.centerXAnchor.constraint(equalTo: refreshControl.centerXAnchor)
 		])
 		
-		requestPhotos {
-			//Update the UI when data fetching has completed
-			self.updateUI()
-		}
+		requestData()
 	}
 	
 	//MARK: Private Methods
-	private func requestPhotos(completion: @escaping () -> ()) {
+	private func requestPhotos(completion: @escaping (Bool, NSError?) -> ()) {
 		let apiRequestLoader = APIRequestLoader(apiRequest: PhotoRequest())
 		apiRequestLoader.loadAPIRequest { [weak self] photos, error in
+			if let error = error as NSError? {
+				completion(false, error)
+			}
 			if let photos = photos {
 				guard let self = self else { return }
 				self.dataSource.photos = photos
-				completion()
+				completion(true, nil)
 			}
 		}
 	}
@@ -82,8 +107,33 @@ final class PhotoVC: UIViewController {
 		}
 	}
 	
+	//Method is used to request the photo data and refresh UI based on response
+	private func requestData() {
+		activityIndicator.startAnimating()
+		requestPhotos { success, error in
+			DispatchQueue.main.async {
+				self.isFetchingData = false
+				self.activityIndicator.stopAnimating()
+				if success {
+					//Update the UI when data fetching has completed
+					self.updateUI()
+				} else {
+					if let error = error {
+						if error.code == -1009 {
+							//Internet is offline
+							self.isOffline = true
+							self.collectionView.reloadData()
+						}
+					}
+				}
+			}
+		}
+		isFetchingData = true
+	}
+	
+	//Used specifically for pull to refresh
 	@objc private func refreshData() {
-		requestPhotos {
+		requestPhotos { success, error in
 			self.updateUI()
 			DispatchQueue.main.async {
 				self.customRefreshView.stop()
@@ -93,6 +143,7 @@ final class PhotoVC: UIViewController {
 	}
 	
 	//IBActions
+	//Used to scroll user to the top of the list
 	@IBAction func topButtonPressed(_ sender: UIBarButtonItem) {
 		collectionView.scrollToItem(at: IndexPath(row: 0, section: 0), at: .top, animated: true)
 	}
@@ -142,6 +193,49 @@ extension PhotoVC: UICollectionViewDelegateFlowLayout {
 		let marginsAndInsets = inset * 2 + collectionView.safeAreaInsets.left + collectionView.safeAreaInsets.right + minimumInteritemSpacing * CGFloat(cellsPerRow - 1)
 		let itemWidth = ((collectionView.bounds.size.width - marginsAndInsets) / CGFloat(cellsPerRow)).rounded(.down)
 		return CGSize(width: itemWidth, height: itemWidth)
+	}
+}
+
+extension PhotoVC: DZNEmptyDataSetSource, DZNEmptyDataSetDelegate {
+	
+	func buttonTitle(forEmptyDataSet scrollView: UIScrollView!, for state: UIControl.State) -> NSAttributedString! {
+		
+		let attributedString = NSAttributedString(string: "Reload Data")
+		return attributedString
+	}
+	
+	func emptyDataSet(_ scrollView: UIScrollView!, didTap button: UIButton!) {
+		requestData()
+	}
+	
+	func title(forEmptyDataSet scrollView: UIScrollView!) -> NSAttributedString! {
+		var attributedString: NSAttributedString =  NSAttributedString(string: "Offline")
+		if isOffline {
+			attributedString = NSAttributedString(string: "Offline")
+		} else {
+			attributedString = NSAttributedString(string: "No Photos")
+		}
+		
+		return attributedString
+	}
+	
+	func description(forEmptyDataSet scrollView: UIScrollView!) -> NSAttributedString! {
+		let attributedString: NSAttributedString
+		if isOffline {
+			attributedString = NSAttributedString(string: "Photos could not be downloaded. Please check your internet connection and try again")
+		} else {
+			attributedString = NSAttributedString(string: "There doesn't seem to be any photos at the moment.")
+		}
+		
+		return attributedString
+	}
+	
+	func emptyDataSetShouldFade(in scrollView: UIScrollView!) -> Bool {
+		return true
+	}
+	
+	func emptyDataSetShouldDisplay(_ scrollView: UIScrollView!) -> Bool {
+		return isFetchingData ? false : true
 	}
 }
 
